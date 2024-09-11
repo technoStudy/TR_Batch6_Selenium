40c6b24529cf958d40b91a25b48882f6effc4087
TechnoStudy <ismettechno@gmail.com>
2024-09-11 19:17:12 +0300

    Gun04 - Select Soru çözümü

--- src/Gun08/_04_Soru.java
+++ src/Gun08/_04_Soru.java
@@ -1,6 +1,13 @@
 package Gun08;
 
-public class _04_Soru {
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class _04_Soru extends BaseDriver {
 //        Test Senaryosu
 //        1- https://www.facebook.com/  sayfasına gidiniz
 //        2- CreateNewAccount a tıklatınız.
@@ -8,6 +15,41 @@ public class _04_Soru {
 //        4- Doğum tarihinizi Select sınıfı ile seçiniz
 
 
+    @Test
+    public void Test1()
+    {
+        driver.get("https://www.facebook.com/");
+
+        WebElement createAccount=driver.findElement(By.xpath("//*[@data-testid='open-registration-form-button']"));
+        createAccount.click();
+
+        MyFunc.Bekle(2);
+
+        WebElement firstName=driver.findElement(By.name("firstname"));
+        firstName.sendKeys("İsmet");
+
+        WebElement lastname=driver.findElement(By.name("lastname"));
+        lastname.sendKeys("Temur");
+
+        WebElement email=driver.findElement(By.name("reg_email__"));
+        email.sendKeys("İsmet@gmail.com");
+
+        MyFunc.Bekle(2);
+
+        WebElement webDay=driver.findElement(By.id("day"));
+        WebElement webMonth=driver.findElement(By.id("month"));
+        WebElement webYear=driver.findElement(By.id("year"));
+
+        Select dayMenu=new Select(webDay);
+        Select monthMenu=new Select(webMonth);
+        Select yearMenu=new Select(webYear);
+
+        dayMenu.selectByValue("23");
+        monthMenu.selectByValue("2");
+        yearMenu.selectByValue("1974");
+
+        BekleKapat();
+    }
 
 
 }
